e971ac64bc5e52975945b218a8701f338223b8f9
Muhammet Mirac <113796231+Muhammetmirac@users.noreply.github.com>
2022-12-20 09:56:43 +0300

    v01

---
+++ src/test/java/day02_DriverMethods/C03_LocatorsGetMethodlari.java
@@ -0,0 +1,8 @@
+package day02_DriverMethods;
+
+public class C03_LocatorsGetMethodlari {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("hello world");
+    }
+}
